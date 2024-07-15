8e1fe44b32e47d058af27ac936d02218cfd18e62
AMANPANDYA114 <141421528+AMANPANDYA114@users.noreply.github.com>
2024-07-15 15:47:28 +0530

    updated server sec

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import cors from "cors"; // Import the cors package
-// import userRoutes from "./routes/user.js";
-// import postRoutes from "./routes/post.js";
-// import cookieParser from "cookie-parser";
-// dotenv.config();
-// const app = express();
-// const port = 8080;
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors"; // Import the cors package
+import userRoutes from "./routes/user.js";
+import postRoutes from "./routes/post.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
+const app = express();
+const port = 8080;
 
 
-// app.use(express.json());
+app.use(express.json());
 
 
 
 
 
-// app.use(cookieParser());
-// app.use('/api/user', userRoutes);
+app.use(cookieParser());
+app.use('/api/user', userRoutes);
 
-// app.use('/api/userpost',postRoutes);
-// // Connect to MongoDB using Mongoose
-// mongoose.connect(process.env.MONGO_DB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }).then(() => {
-//   console.log("Connected to MongoDB");
-//   // Start the server after successful database connection
-//   app.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`);
-//   });
-// }).catch((err) => {
-//   console.error("Error connecting to MongoDB:", err);
-// });
+app.use('/api/userpost',postRoutes);
+// Connect to MongoDB using Mongoose
+mongoose.connect(process.env.MONGO_DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+  // Start the server after successful database connection
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+});
 
 
 
@@ -83,36 +83,3 @@
 
 
 
-
-
-
-
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRoutes from "./routes/user.js";
-import postRoutes from "./routes/post.js";
-import cookieParser from "cookie-parser";
-
-dotenv.config();
-const app = express();
-const port = 8080;
-
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api/user', userRoutes);
-app.use('/api/userpost', postRoutes);
-
-// Connect to MongoDB using Mongoose
-mongoose.connect(process.env.MONGO_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("Connected to MongoDB");
-    // Start the server after successful database connection
-    app.listen(port, () => {
-        console.log(`Server is listening on port ${port}`);
-    });
-}).catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-});
